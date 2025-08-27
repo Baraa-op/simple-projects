6a1294146891289321482a1f2cd1a0673484d6c2
Baraa <156128855+Baraa-op@users.noreply.github.com>
2025-08-27 14:51:12 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+
+
+int main()
+{
+	for(int i=1 ; i<=10 ; i++)
+	{
+		cout <<"\n\n******************************\n\n" ;
+		for(int f=1 ; f<=10 ; f++)
+		{
+			
+			cout << i << " × " << f << " = " << i*f << endl;
+		}
+		
+	}
+}
+q
